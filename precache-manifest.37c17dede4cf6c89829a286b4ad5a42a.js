82ff2fac3f0ae0d1628c5e2a3d2de9ae75734852
Joe Baranski <joe.baranski49@gmail.com>
2020-04-04 13:26:11 -0400

    Updates

---
+++ precache-manifest.37c17dede4cf6c89829a286b4ad5a42a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19766cc77d379bb27339a6c98dd8a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c383b3fcca6e6cb7e33a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87eaea2f66dfeb33595a",
+    "url": "/static/js/2.c7a72bd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7a72bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c383b3fcca6e6cb7e33a",
+    "url": "/static/js/main.da99c072.chunk.js"
+  },
+  {
+    "revision": "99e9ccffef11f8ec262a",
+    "url": "/static/js/runtime-main.e4bf3335.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
